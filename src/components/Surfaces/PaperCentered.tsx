6347979f00654a1424358dcdb65de89a56021fe8
jmwalsh91 <jordanmwalsh@gmail.com>
2022-09-15 19:05:11 -0600

    task: PaperCentered component

---
+++ src/components/Surfaces/PaperCentered.tsx
@@ -0,0 +1,18 @@
+import { Center, Paper } from '@mantine/core'
+import React from 'react'
+
+type Props = {
+    children: React.ReactNode
+}
+
+function PaperCentered({children}: Props) {
+  return (
+    <Paper>
+        <Center>
+            {children}
+        </Center>
+    </Paper>
+  )
+}
+
+export default PaperCentered
