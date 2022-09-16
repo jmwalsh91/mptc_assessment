d7f717a5de16ddbe867038db7e847462704a1f10
jmwalsh91 <jordanmwalsh@gmail.com>
2022-09-15 19:00:00 -0600

    init: start timer, boiler, create repo, gitignore

---
+++ src/utils/tests/index.tsx
@@ -0,0 +1,25 @@
+import {
+  RenderOptions,
+  RenderResult,
+  cleanup,
+  render,
+} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { ReactElement } from 'react'
+import { afterEach } from 'vitest'
+
+afterEach(() => {
+  cleanup()
+})
+
+const customRender = (
+  ui: ReactElement,
+  options: RenderOptions = {}
+): RenderResult =>
+  render(ui, {
+    wrapper: ({ children }) => <div>{children}</div>,
+    ...options,
+  })
+
+export * from '@testing-library/react'
+export { customRender as render, userEvent }
